fa41151f06006b2dc9e5f1cb2c65aa12fcc8ecae
Szymon Winiarek <s27577@pjwstk.edu.pl>
2024-04-07 15:27:42 +0200

    Exercise 2

---
+++ PGO.1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student st = new Student();
+        st.fname = "Mateusz";
+        st.lname = "S.";
+        st.indexNumber = 27577;
+        st.email = "xyz@gmail.com";
+        st.address = "xyz";
+        st.grades = new double[]
+
+                
+    }
+}
---
+++ PGO.1/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public String fname;
+    public String lname;
+    public int indexNumber;
+    public String email;
+    public String address;
+    public double grades;
+}
