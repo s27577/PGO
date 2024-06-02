d26cef2a0e1bef8aca893430f0e06e92cba21bce
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:09:12 +0200

    Update Student.java

--- PGO.1/src/Student.java
+++ PGO.1/src/Student.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
-    public String fname;
-    public String lname;
-    public int indexNumber;
-    public String email;
-    public String address;
-    public double grades;
+    //zmienne
+    private String firstName;
+    private String lastName;
+    private String indexNumber;
+    private String email;
+    private String address;
+    private List<Double> grades;
+
+    //konstruktor
+    public Student(String firstName, String lastName, String indexNumber, String email, String address, List<Double> grades) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.indexNumber = indexNumber;
+        this.email = email;
+        this.address = address;
+        this.grades = grades;
+    }
+
+    //metody
+    public double calculateGradesAverage() {
+        double sum = 0;
+        double average = 0;
+        if (grades.isEmpty()) {
+            throw new IllegalArgumentException("Student has no grades");
+        } else if (grades.size() > 20) {
+            throw new IllegalArgumentException("Student has more than 20 grades");
+        } else {
+            for (double grade : grades) {
+                sum += grade;
+            }
+            average = sum / grades.size();
+        }
+        return roundToNearestHalf(average);
+    }
+    private double roundToNearestHalf(double value) {
+        return Math.round(value * 2) / 2.0;
+    }
 }
