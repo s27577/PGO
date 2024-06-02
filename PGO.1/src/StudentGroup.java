558b5c4eac0120eefbf658a218144c50cb394e3f
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:09:34 +0200

    Update StudentGroup.java

--- PGO.1/src/StudentGroup.java
+++ PGO.1/src/StudentGroup.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentGroup {
-    public String nazwa;
+    //zmienne
+    private String groupName;
+    private List<Student> students;
+
+    //konstruktor
+    public StudentGroup(String groupName) {
+        this.groupName = groupName;
+        this.students = new ArrayList<>();
+    }
+
+    //metody
+    public void addStudent(Student student) {
+        if (students.size() >= 15) {
+            throw new IllegalStateException("Maximum amount of students in the group");
+        }
+        if (students.contains(student)) {
+            throw new IllegalStateException("Student is already in the group");
+        }
+        students.add(student);
+    }
+
 }
